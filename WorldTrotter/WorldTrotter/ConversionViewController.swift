23d0f9403e8b756f290fc36b3c38517d13ec2010
Antonio Medrano <tonedabass@gmail.com>
2017-03-07 00:33:04 -0800

    Added default text response text when textfield is empty

---
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Antonio Medrano on 3/6/17.
+//  Copyright © 2017 Antonio Medrano. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, !text.isEmpty {
+            celsiusLabel.text = text
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+
+}
