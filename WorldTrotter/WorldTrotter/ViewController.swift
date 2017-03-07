35ee87521c4b50d127930c35bd4f85ff062adace
Antonio Medrano <tonedabass@gmail.com>
2017-03-06 23:35:21 -0800

    More work with learning Auto Layout and constraints

--- WorldTrotter/WorldTrotter/ViewController.swift
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -10,18 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
-        let firstView = UIView(frame: firstFrame)
-        firstView.backgroundColor = UIColor.blue
-        view.addSubview(firstView)
-        
-        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
-        let secondView = UIView(frame: secondFrame)
-        secondView.backgroundColor = UIColor.green
-        firstView.addSubview(secondView)
-    }
+
 }
 
